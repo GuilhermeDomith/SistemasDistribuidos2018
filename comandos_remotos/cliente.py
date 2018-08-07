6173daffb6a39c334f6681bc20c6843cd6c9545b
Guilherme Domith <guilhermedomith@gmail.com>
2018-08-07 09:12:23 -0300

    Atualização

--- comandos_remotos/cliente.py
+++ comandos_remotos/cliente.py
@@ -2,20 +2,22 @@ import socket as s
 import servidor
 
 socket = s.socket(s.AF_INET, s.SOCK_STREAM)
-socket.connect( ('localhost', 25000) )
+socket.connect(('localhost', 25000))
 
 while True:
 	try:
 		comando = input('comando: ')
 		if comando.lower() == 'exit':
-			break;
+			break
 
+		socket.send(comando.encode('utf-8'))
+		resposta = socket.recv(1024)
+		print(resposta.decode('utf-8'))
 
-			socket.send(comando.encode('utf-8'))
-			resposta = socket.recv(1024)
-			print(resposta.decode('utf-8'))
-	except KeyboardInterrupt: break
-	except: break
+	except KeyboardInterrupt:
+		break
+	except:
+		break
 
 print('Conexão encerrada')
 socket.close()
--- comandos_remotos/servidor.py
+++ comandos_remotos/servidor.py
@@ -1,42 +1,47 @@
 import socket as s
 import subprocess
 
+
 def iniciar():
-	socket = s.socket(s.AF_INET, s.SOCK_STREAM)
-	socket.bind( ('localhost', 25000) )
-	socket.listen(1)
+    socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+    socket.bind(('localhost', 25000))
+    socket.listen(1)
 
-	while True:
-		try:
-			print('Esperando conexão...')
-			connection, addr = socket.accept()
-			print('Conexão aceita.')
-			atenderCliente(connection)
-		except KeyboardInterrupt: break
+    while True:
+        try:
+            print('Esperando conexão...')
+            connection, addr = socket.accept()
+            print('Conexão aceita.')
+            atenderCliente(connection)
+        except KeyboardInterrupt:
+            break
 
-	print('Servidor encerrado.')
-	socket.close();
+    print('Servidor encerrado.')
+    socket.close()
 
 
 def atenderCliente(connection):
 
-	while True:
-		comando = connection.recv(1024).decode('utf-8')
+    while True:
+        comando = connection.recv(1024).decode('utf-8')
 
-		if comando.lower() == 'exit':
-			break
+        if comando.lower() == 'exit':
+            break
 
-		try:
-			resultado_cmd = subprocess.check_output(comando.split(' '),
-							universal_newlines=True,
-							stderr=subprocess.STDOUT)
-		except FileNotFoundError: resultado_cmd = 'Comando não encontrado.'
-		except: break
+        try:
+            resultado_cmd = subprocess.check_output(comando.split(' '),
+                                                    universal_newlines=True,
+                                                    stderr=subprocess.STDOUT)
+        except FileNotFoundError:
+            resultado_cmd = 'Comando não encontrado.'
+        except:
+            break
 
-		connection.send(resultado_cmd.encode('utf-8'))
+        connection.send(resultado_cmd.encode('utf-8'))
 
-	connection.close();
-	print('Conexão encerrada')
+    connection.close()
+    print('Conexão encerrada')
+
 
 if __name__ == '__main__':
-	iniciar()
+    iniciar()
